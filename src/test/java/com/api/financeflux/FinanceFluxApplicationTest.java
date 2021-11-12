e38ef008a5c915f5968a08c043947685ccf8ec51
Carlos Machado <carlos.c.junior@capgemini.com>
2021-11-12 09:18:50 -0300

    teste de cobertura na classe principal

---
+++ src/test/java/com/api/financeflux/FinanceFluxApplicationTest.java
@@ -0,0 +1,12 @@
+package com.api.financeflux;
+
+import org.junit.jupiter.api.Test;
+
+class FinanceFluxApplicationTest {
+
+	@Test
+	void main() {
+		FinanceFluxApplication.main(new String[] {});
+	}
+
+}
